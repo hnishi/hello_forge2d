e03d6330cf53c085d784cc833778678aba569f73
hnishi <nishigami2509@gmail.com>
2022-01-02 16:29:13 +0900

    Add space craft

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,8 @@ import 'package:flame/game.dart';
 import 'package:flame/components.dart';
 import 'package:flame/input.dart';
 
-class Player extends PositionComponent {
-  static final _paint = Paint()..color = Colors.white;
-
-  @override
-  void render(Canvas canvas) {
-    canvas.drawRect(size.toRect(), _paint);
-  }
-
-  void move(Vector2 delta){
+class Player extends SpriteComponent {
+  void move(Vector2 delta) {
     position.add(delta);
   }
 }
@@ -20,20 +13,23 @@ class SpaceShooterGame extends FlameGame with PanDetector {
   late Player player;
 
   @override
-  Future<void> onLoad() async {
+  Future<void>? onLoad() async {
     await super.onLoad();
 
-    add(
-      player = Player()
-        ..position = size / 2
-        ..width = 50
-        ..height = 100
-        ..anchor = Anchor.center,
-    );
+    final playerSprite = await loadSprite('player-sprite.png');
+    player = Player()
+      ..sprite = playerSprite
+      ..x = size.x / 2
+      ..y = size.y / 2
+      ..width = 50
+      ..height = 100
+      ..anchor = Anchor.center;
+
+    add(player);
   }
 
   @override
-  void onPanUpdate(DragUpdateInfo info){
+  void onPanUpdate(DragUpdateInfo info) {
     player.move(info.delta.game);
   }
 }
