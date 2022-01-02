d39e3d511439a178f9bdc3b55e58480f89e8e81e
hnishi <nishigami2509@gmail.com>
2022-01-02 17:19:40 +0900

    Use keyboard control (WASD)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
 import 'package:flame/input.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 class Player extends SpriteComponent with HasGameRef<SpaceShooterGame> {
   @override
@@ -21,7 +23,10 @@ class Player extends SpriteComponent with HasGameRef<SpaceShooterGame> {
   }
 }
 
-class SpaceShooterGame extends FlameGame with PanDetector {
+class SpaceShooterGame extends FlameGame with KeyboardEvents {
+  static const int speed = 200;
+  final Vector2 velocity = Vector2(0, 0);
+
   late Player player;
 
   @override
@@ -34,8 +39,30 @@ class SpaceShooterGame extends FlameGame with PanDetector {
   }
 
   @override
-  void onPanUpdate(DragUpdateInfo info) {
-    player.move(info.delta.game);
+  void update(double dt) {
+    super.update(dt);
+    final displacement = velocity * (speed * dt);
+    player.move(displacement);
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+    RawKeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
+    final isKeyDown = event is RawKeyDownEvent;
+
+    if (event.logicalKey == LogicalKeyboardKey.keyA) {
+      velocity.x = isKeyDown ? -1 : 0;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyD) {
+      velocity.x = isKeyDown ? 1 : 0;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyW) {
+      velocity.y = isKeyDown ? -1 : 0;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyS) {
+      velocity.y = isKeyDown ? 1 : 0;
+    }
+
+    return super.onKeyEvent(event, keysPressed);
   }
 }
 
